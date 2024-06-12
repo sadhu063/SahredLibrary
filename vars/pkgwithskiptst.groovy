4a8b1114147be8f21938b1e917dec4989cb9dbe1
Devangam <92856944+Devangam@users.noreply.github.com>
2024-06-12 16:03:54 +0530

    Update pkgwithskiptst.groovy

--- vars/pkgwithskiptst.groovy
+++ vars/pkgwithskiptst.groovy
@@ -4,7 +4,7 @@ def call() {
      sh 'mvn package -DskipTests=true' 
 
      //Define the maven command to be executed
-     def maven command = 'mvn package -DskipTests=true' 
+     def mavenCommand = 'mvn package -DskipTests=true' 
 
      //Execute the maven command in a shell
      sh script: maven command, returnStatus: true     
